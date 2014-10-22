9a3b874aa626c615a07c92b50a0673902c674965
Steve DiBenedetto <dibenede@cs.colostate.edu>
2014-10-22 11:17:01 -0400

    Explicitly provide key in copy-solutions.sh

--- tools/copy-solutions.sh
+++ tools/copy-solutions.sh
@@ -2,13 +2,13 @@
 
 source ./ndn-tutorial-config.sh
 
-scp -P `echo $UCLA_1 | cut -d ':' -f2` ../solutions/hello_consumer.py $USERNAME@$GENI_HOST:~/
-scp -P `echo $UCLA_1 | cut -d ':' -f2` ../solutions/hello_ext_consumer.py $USERNAME@$GENI_HOST:~/
-scp -P `echo $UCLA_2 | cut -d ':' -f2` ../solutions/hello_ext_consumer.py $USERNAME@$GENI_HOST:~/
-scp -P `echo $UCLA_2 | cut -d ':' -f2` ../solutions/hello_consumer.py $USERNAME@$GENI_HOST:~/
-scp -P `echo $UCLA_2 | cut -d ':' -f2` producer.py $USERNAME@$GENI_HOST:~/
-scp -P `echo $UCLA_1 | cut -d ':' -f2` producer.py $USERNAME@$GENI_HOST:~/
-scp -P `echo $CSU_1 | cut -d ':' -f2` consumer.py $USERNAME@$GENI_HOST:~/
-scp -P `echo $CSU_1 | cut -d ':' -f2` ../solutions/hello_producer.py $USERNAME@$GENI_HOST:~/
-scp -P `echo $CSU_1 | cut -d ':' -f2` ../solutions/hello_ext_producer.py $USERNAME@$GENI_HOST:~/
-scp -P `echo $UCLA_HUB | cut -d ':' -f2` ../solutions/*.{cpp,hpp} $USERNAME@$GENI_HOST:~/
+scp -i $KEY -P `echo $UCLA_1 | cut -d ':' -f2` ../solutions/hello_consumer.py $USERNAME@$GENI_HOST:~/
+scp -i $KEY -P `echo $UCLA_1 | cut -d ':' -f2` ../solutions/hello_ext_consumer.py $USERNAME@$GENI_HOST:~/
+scp -i $KEY -P `echo $UCLA_2 | cut -d ':' -f2` ../solutions/hello_ext_consumer.py $USERNAME@$GENI_HOST:~/
+scp -i $KEY -P `echo $UCLA_2 | cut -d ':' -f2` ../solutions/hello_consumer.py $USERNAME@$GENI_HOST:~/
+scp -i $KEY -P `echo $UCLA_2 | cut -d ':' -f2` producer.py $USERNAME@$GENI_HOST:~/
+scp -i $KEY -P `echo $UCLA_1 | cut -d ':' -f2` producer.py $USERNAME@$GENI_HOST:~/
+scp -i $KEY -P `echo $CSU_1 | cut -d ':' -f2` consumer.py $USERNAME@$GENI_HOST:~/
+scp -i $KEY -P `echo $CSU_1 | cut -d ':' -f2` ../solutions/hello_producer.py $USERNAME@$GENI_HOST:~/
+scp -i $KEY -P `echo $CSU_1 | cut -d ':' -f2` ../solutions/hello_ext_producer.py $USERNAME@$GENI_HOST:~/
+scp -i $KEY -P `echo $UCLA_HUB | cut -d ':' -f2` ../solutions/*.{cpp,hpp} $USERNAME@$GENI_HOST:~/
